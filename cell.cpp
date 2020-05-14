9fffb3a21fc532f5b2553f91f32984347c088b49
A.N.Gulchenko <a.n.gulchenko@gmail.com>
2020-05-14 09:08:21 +0300

    Cell must be much more clever

---
+++ cell.cpp
@@ -0,0 +1,24 @@
+﻿#include "cell.h"
+
+Cell::Cell()
+{
+
+}
+
+void Cell::resetSubValues()
+{
+	subvalues = {true, true, true, true, true, true};
+}
+
+int Cell::countSubValues()
+{
+	int res = 0;
+	for (bool subvalue: subvalues)
+	{
+		if (subvalue == true)
+		{
+			res++;
+		}
+	}
+	return res;
+}
---
+++ cell.h
@@ -0,0 +1,20 @@
+﻿#ifndef CELL_H
+#define CELL_H
+
+#include <array>
+
+class Cell
+{
+public:
+	Cell();
+	void resetSubValues();
+	int countSubValues();
+
+	int value;
+	bool player_knows_value = false;
+	std::array<bool, 6> subvalues = {true, true, true, true, true, true};
+	int row;
+	int col;
+};
+
+#endif // CELL_H
